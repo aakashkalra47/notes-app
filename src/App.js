43d556e4432cf4e4a981178af746b5aa58adf07f
aakashkalra47 <aakashkalra47@gmail.com>
2022-02-28 02:47:56 +0530

    fixed background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 // import CreateNote from "./Components/CreateNote";
 import NotesList from "./Components/NoteList";
 import CreateNote from "./Components/create";
-import Filters from "./Components/Filters";
 function App() {
   return (
     <div className="container">
       <div style={{ display: "flex",flex:1, marginBottom: 10 }}>
-        {/* <div style={{ margin: "0px 20px" }}> */}
           <CreateNote />
-        {/* </div> */}
-        {/* <div style={{margin: "0px 20px", marginLeft:'auto' }}>
-          <Filters />
-        </div> */}
       </div>
       <div>
         <NotesList />
